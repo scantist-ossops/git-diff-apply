9e75293a20b48f1c9ed4638917c55bb33c4fbf16
jiongle <jiongle@scantist.com>
2023-11-16 15:56:38 +0800

    Patch vulnerability in older version

--- src/index.js
+++ src/index.js
@@ -249,7 +249,7 @@ module.exports = async function gitDiffApply({
     _tmpDir = await tmpDir();
     tmpWorkingDir = _tmpDir;
 
-    await utils.run(`git clone ${remoteUrl} ${_tmpDir}`);
+const { runWithSpawn } = require('./run');
 
     returnObject = await buildReturnObject();
 
